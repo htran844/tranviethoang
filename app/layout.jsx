e0063bd39d8dca12c83de70c59d86e093aac7d6a
htran844 <tran.viet.hoang844@gmail.com>
2023-07-25 16:44:07 +0700

    change logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,22 @@ import '@styles/globals.css'
 import SearchNav from "@components/SearchNav";
 import Header from '@components/Header';
 
+export const metadata = {
+   title: "Phòng học thứ n",
+//    icons: {
+//     icon: [
+//         // "/favicon.ico?v=4"
+//         "https://www.sony.com.vn/image/ccbe548efa94995a9c034d96a4a6acbb?fmt=pjpeg&bgcolor=FFFFFF&bgc=FFFFFF&wid=2515&hei=1320"
+//     ],
+//     apple: [
+//         "/apple-touch-icon.png?v=4"
+//     ],
+//     shortcut: [
+//         "/apple-touch-icon.png"
+//     ]
+//    },
+//    manifest: "/site.webmanifest"
+  };
 
 const RootLayout = ({ children }) => {
     return (
--- components/Header.jsx
+++ components/Header.jsx
@@ -2,13 +2,19 @@
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 import "../styles/style-range.css"
+import Image from 'next/image'
 
 const Header = () => {
     return (
-        <div className='py-6'>
-            ----
-            <span className='orange_gradient text-center text-3xl py-5'> tranviethoang.com</span>
-            ----
+        <div className='py-6 flex justify-center'>
+            {/* <span className='orange_gradient text-center text-3xl py-5'> tranviethoang.com</span> */}
+            <Image
+                src="/assets/images/logo.png"
+                width={300}
+                height={300}
+                alt="Picture of the author"
+                className=''
+            />
         </div>
 
 
