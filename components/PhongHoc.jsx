2be1fbf5e11fff40bcd52309a0bbf2bc6ab28b43
htran844 <tran.viet.hoang844@gmail.com>
2023-08-02 16:34:18 +0700

    update

--- components/PhongHoc.jsx
+++ components/PhongHoc.jsx
@@ -1,13 +1,28 @@
+import Image from "next/image"
 
 
 function PhongHoc() {
   return (
-    <div className="flex justify-center items-center">
-        <span>
-        Cùng học React Native với mình. Liên hệ qua FB: 
-        </span>
-        <a href="https://www.facebook.com/h7844/" className="text-blue-600">Việt Hoàng</a>
+    <div>
+      <div className="flex flex-col justify-between items-center mr-4">
+        <div>
+          Học React Native từ đầu hoặc bổ sung kiến thức trước khi xin intern/fresher bằng việc học 1-1 với mình. Liên hệ qua FB:
+        </div>
+        <div>
+          <a href="https://www.facebook.com/h7844/" className="text-blue-600">Việt Hoàng</a>
+        </div>
+      </div>
+      <div className="flex items-center justify-center mt-5">
+        <Image
+          src="/assets/images/fb.jpg"
+          width={300}
+          height={500}
+          alt="Picture of the author"
+          className=''
+        />
+      </div>
     </div>
+
   )
 }
 
